dda41f259a7c52dc02edc099df39fd4515571eea
Stefan Hübner <sthuebner@googlemail.com>
2011-09-24 14:59:06 +0200

    approaching the perfect game (not successfull yet)

--- bowling_game.clj
+++ bowling_game.clj
@@ -11,29 +11,44 @@
 (facts "about frame scores"
   (score-frame [0 0]) => 0
   (score-frame [1 1]) => 2
-  (score-frame [5 5]) => 10
-  )
+  (score-frame [5 5]) => 10)
+
+
 
 (defn as-frames
   [game-rolls]
-  (partition 2 game-rolls)
-  )
+  (partition 2 game-rolls))
 
-(fact
+(fact "about framing"
   (as-frames []) => []
   (as-frames (repeat 20 0)) => (repeat 10 [0 0])
-  (as-frames [10 0 5 3]) => [ [10 0] [5 3] ]
-  )
+  (as-frames [10 0 5 3]) => [ [10 0] [5 3] ])
+
+
 
 (defn strike-frame? [frame-rolls]
   (= [10 0] frame-rolls))
+
+(facts "about strikes"
+  (strike-frame? [0 0]) => false
+  (strike-frame? [5 5]) => false
+  (strike-frame? [10 0]) => true)
+
+
 
 (defn spare-frame? [frame-rolls]
   (and (= 10 (reduce + frame-rolls))
        (not (strike-frame? frame-rolls))))
 
+(facts "about spares"
+  (spare-frame? [0 0]) => false
+  (spare-frame? [5 5]) => true
+  (spare-frame? [10 0]) => false)
+
+
 
 (defn score-it [[current-score score-fn] next-frame]
+  (println current-score)
   (vector
    (+ current-score (score-fn next-frame))
    ;; producing the next score-fn
@@ -43,18 +58,13 @@
     (spare-frame? next-frame) (fn [frame-rolls]
 				(apply + (* 2 (first frame-rolls))
 				       (next frame-rolls)))
-    :else score-frame))
-  )
+    :else score-frame)))
 
 (defn score-game [game-rolls]
   ;; idea: don't even carry state but just functions (like continuations)
   (first (reduce score-it [0 score-frame] (take 10 (as-frames game-rolls)))))
 
 
-
-(facts "about spares"
-  (spare-frame? [0 0]) => false
-  (spare-frame? [5 5]) => true)
 
 (facts "about Bowling games"
   (fact "scoring a gutter game"
@@ -67,4 +77,6 @@
     (score-game (concat [5 5 3 7 3] (repeat 0))) => 29)
   (fact "scoring a game of 1 strike, an 8 pin frame, and gutters"
     (score-game (concat [10 0 5 3] (repeat 0))) => 26)
+  (future-fact "scoring a perfect game"
+    (score-game (cycle [10 0])) => 300)
   )
