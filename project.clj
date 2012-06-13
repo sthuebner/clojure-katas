3826a4900433aa252d1e36542571d952473412b6
Stefan Hübner <stefan.hubner@nokia.com>
2012-06-13 18:35:25 +0200

    leiningen project.clj
    Conflicts:
    	project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clojure-katas "1-SNAPSHOT"
+
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [midje "1.4.0"]]
+
+  )
+
+;; further, 'lein plugin install swank-clojure 1.3.2' will get you the SWANK plugin
