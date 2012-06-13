2d1ddcff0c7cc04399214f8000cb0dde3f61102b
Stefan Hübner <stefan.hubner@nokia.com>
2012-06-13 18:33:01 +0200

    outcome of today's session

--- src/string_calculator.clj
+++ src/string_calculator.clj
@@ -4,23 +4,38 @@
   (:use [clojure.string :only [split]]
         [midje sweet]))
 
-(defn add [input]
-  (apply + (map #(Integer/parseInt
-                  (if (= "" %) "0" %))
-                (split input #","))))
+(defn find-separator [input]
+  (if (.startsWith input "//")
+    [(.substring input 4)
+     (re-pattern (str ",|\\n|" (.substring input 2 3)))]
+    [input #",|\n"])
+   )
 
+(defn add [input]
+  (apply + (map (fn [s]
+                  (Integer/parseInt (if (= "" s) "0" s)))
+                (apply split (find-separator input)))))
+
+
+(defn new-random []
+  (-> (Math/random) (* 1000) Math/ceil int))
 
 (fact "basics"
   (add "") => 0
   (add "1") => 1
   
-  (let [n (-> (Math/random) (* 1000) Math/ceil int)
-        s (str n)]
-    (add s) => n)
-
-  
-  )
+  (let [n (new-random)]
+    (add (str n)) => n))
 
 (fact "separate numbers by commas"
   (add "1,2") => 3
   (add ",1,2,") => 3)
+
+(fact "support NL as delimiter"
+  (add "1\n2,3") => 6
+  )
+
+
+(fact "support more delimiters"
+  (add "//;\n1;2,3") => 6
+  )
